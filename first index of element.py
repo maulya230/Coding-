b0390e5f54e13480a1887f35e479a815a0af4846
maulya230 <75597705+maulya230@users.noreply.github.com>
2022-07-31 10:06:02 +0530

    Create first index of element.py

---
+++ first index of element.py
@@ -0,0 +1,40 @@
+
+
+n=int(input())
+a=[]
+a=input().split()
+index=-1
+x=input()
+
+for i in range(0,n):
+    if a[i]==x:
+       index=i
+       break
+print(index)
+
+
+    
+#Your code goes here.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
