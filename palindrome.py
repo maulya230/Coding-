7aca8571c8285944427427fef82a6e63ec301595
maulya230 <75597705+maulya230@users.noreply.github.com>
2022-08-06 10:25:25 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        rev=str(x)[::-1]
+        if rev==str(x):
+            return True
+        else:
+            return False
